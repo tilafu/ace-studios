7bfdbb34cec0e6d8364f56466b1572d6506f2d4a
tilafu <tilafumusic@gmail.com>
2023-01-08 21:43:34 +0300

    name change

---
+++ ace-beta/Ace-design/ajax-email.php
@@ -0,0 +1,50 @@
+<?php
+
+/* SETTINGS */
+// $recipient = "acedesigns.ke@gmail.com";
+// $subject = "New Message from Contact Form";
+
+
+
+if(isset($_POST['btn'])){
+{
+  echo "<script type='text/javascript'>
+        console.log('Working');
+    </script>";
+}
+
+  /* DATA FROM HTML FORM */
+  // $name = $_POST['name'];
+  // $email = $_POST['email'];
+  // $message = $_POST['msg'];
+//$phone = $_POST['phone'];
+
+
+  /* SUBJECT */
+  // $emailSubject = $subject . " by " . $name;
+
+  /* HEADERS */
+  // $headers = "From: $name <$email>\r\n" .
+  //            "Reply-To: $name <$email>\r\n" . 
+  //            "Subject: $emailSubject\r\n" .
+  //            "Content-type: text/plain; charset=UTF-8\r\n" .
+  //            "MIME-Version: 1.0\r\n" . 
+  //            "X-Mailer: PHP/" . phpversion() . "\r\n";
+ 
+  // /* PREVENT EMAIL INJECTION */
+  // if ( preg_match("/[\r\n]/", $name) || preg_match("/[\r\n]/", $email) ) {
+  //   header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+  //   die("500 Internal Server Error");
+//   }
+
+//   /* MESSAGE TEMPLATE */
+//   $mailBody = "Name: $name \n\r" .
+//               "Email:  $email \n\r" .
+//               "Subject:  $subject \n\r" .
+// //            "Phone:  $phone \n\r" .
+//               "Message: $message";
+
+//   /* SEND EMAIL */
+//   mail($recipient, $emailSubject, $mailBody, $headers);
+}
+?>
